c4eca9cc44d40ea87164a5f3620db664dc62b3a2
Antonio Esper <a01202743@itesm.mx>
2016-11-08 09:44:03 -0600

    class Node init

--- node.py
+++ node.py
@@ -1,1 +1,7 @@
-# antonio & angel
+class Node(object):
+    def __init__(self):
+        self.name = None
+        self.children = {}
+        self.column = None
+        self.leaf = False
+        self.classes = None
