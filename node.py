47a17237dd5f05ae07db735f437c5c7bd0e8d768
Angel Flores <aaflores93@gmail.com>
2016-11-08 09:51:00 -0600

    finished node def

--- node.py
+++ node.py
@@ -5,3 +5,19 @@ class Node(object):
         self.column = None
         self.leaf = False
         self.classes = None
+    def answer(self, level=0, ):
+        result = ""
+
+        if not self.leaf:
+            for key, val in self.children.iteritems():
+                result += "\t" * level
+                result += self.name + ": " + key + "\n"
+                result += val.answer(level + 1)
+        else:
+            result += "  " * level
+            result += "answer: " + self.classes.keys()[0] + '\n'
+
+        return result
+
+    def __repr__(self):
+        return self.answer()
